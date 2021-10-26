03948d04ab0505b63935a81b505c59459deb3936
Alisher Muzaffarov <49343639+aliml92@users.noreply.github.com>
2021-10-26 18:18:50 +0900

    new version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name='mpegdash',
-    version='0.0.5',
+    version='0.0.6',
     author='Alisher M',
     author_email='alisherm@test.com',
     description='Super simple wrapper around shaka-packager',
