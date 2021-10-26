bf776f8e963e8430ddea1fa9e6a1db477b29df78
aliml92 <almowins5yearsss@gmail.com>
2021-10-26 13:38:37 +0900

    d

--- setup.py
+++ setup.py
@@ -4,16 +4,16 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name='mpegdashprep',
+    name='mpegdash',
     version='0.0.1',
     author='Alisher M',
     author_email='alisherm@test.com',
     description='Super simple wrapper around shaka-packager',
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url='https://github.com/aliml92/mpeg-dash-prep',
+    url='https://github.com/aliml92/mpegdash',
     project_urls = {
-        "Bug Tracker": "https://github.com/aliml92/mpeg-dash-prep/issues"
+        "Bug Tracker": "https://github.com/aliml92/mpegdash/issues"
     },
     license='MIT',
     packages=['mpegdashprep'],
